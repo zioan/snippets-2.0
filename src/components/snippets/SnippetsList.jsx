b7f872c06c320194abfaf757e9a775aab991a00c
zioan <zaharia.ioan@gmail.com>
2023-02-20 18:03:50 +0100

    lazy loading snippets for improving performance

--- src/components/snippets/SnippetsList.jsx
+++ src/components/snippets/SnippetsList.jsx
@@ -1,18 +1,61 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useRef, useState } from 'react'
 import SnippetContext from '../../context/SnippetContext'
 import SnippetTemplate from './SnippetTemplate'
 
 function SnippetsList() {
-  const { snippets, getSnippets, searchedSnippets, loading } = useContext(
-    SnippetContext,
-  )
+  const { searchedSnippets } = useContext(SnippetContext)
+
+  const [renderedData, setRenderedData] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+  const [currentPage, setCurrentPage] = useState(1)
+  const loadThreshold = 1000 // define a threshold for how far from the bottom of the page to load more data
+  const containerRef = useRef(null)
+
+  useEffect(() => {
+    setRenderedData(searchedSnippets.slice(0, currentPage * 10)) // render the first 10 items from the data
+  }, [searchedSnippets, currentPage])
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const { scrollHeight, scrollTop, clientHeight } = document.documentElement
+      const distanceFromBottom = scrollHeight - (scrollTop + clientHeight)
+
+      if (distanceFromBottom < loadThreshold && !isLoading) {
+        setIsLoading(true)
+        setCurrentPage((prevPage) => prevPage + 1)
+      }
+    }
+
+    window.addEventListener('scroll', handleScroll)
+    return () => window.removeEventListener('scroll', handleScroll)
+  }, [isLoading])
+
+  useEffect(() => {
+    if (!isLoading) return
+
+    // Simulate fetching more data from the server
+    const fetchMoreData = () => {
+      setTimeout(() => {
+        setRenderedData((prevData) => [
+          ...prevData,
+          ...searchedSnippets.slice(prevData.length, currentPage * 10),
+        ])
+        setIsLoading(false)
+      }, 1000)
+    }
+
+    fetchMoreData()
+  }, [isLoading, searchedSnippets, currentPage])
 
   return (
-    <>
-      {searchedSnippets.map((snippet) => {
-        return <SnippetTemplate key={snippet.id} snippet={snippet} />
-      })}
-    </>
+    <div>
+      {renderedData.map((snippet) => (
+        <SnippetTemplate key={snippet.id} snippet={snippet} />
+      ))}
+      <div ref={containerRef} style={{ height: '10px' }}>
+        {isLoading && <div>Loading...</div>}
+      </div>
+    </div>
   )
 }
 
