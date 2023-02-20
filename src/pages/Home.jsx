87d465cb066cf9e65b8039eb2b1dc521dd839287
zioan <zaharia.ioan@gmail.com>
2023-02-20 16:50:41 +0100

    cleanup

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,9 @@
-import { useContext, useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { useContext } from 'react'
 import Auth from '../components/layout/Auth'
 import AuthContext from '../context/AuthContext'
-import waitForServerResponse from '../helpers/checkForUser'
 
 function Home() {
   const { user } = useContext(AuthContext)
-  const navigate = useNavigate()
-
-  useEffect(() => {
-    waitForServerResponse().then(
-      (response) => response && navigate('/snippets'),
-    )
-  }, [])
 
   return <>{!user && <Auth />}</>
 }
