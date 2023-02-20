e41a94fc5efdf8349aa7bc0ce9b028fc797d73ae
zioan <zaharia.ioan@gmail.com>
2023-02-20 16:57:52 +0100

    addded More features message on homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,12 @@ import AuthContext from '../context/AuthContext'
 function Home() {
   const { user } = useContext(AuthContext)
 
-  return <>{!user && <Auth />}</>
+  return (
+    <>
+      {!user && <Auth />}
+      {user && <p>More features are coming soon.</p>}
+    </>
+  )
 }
 
 export default Home
