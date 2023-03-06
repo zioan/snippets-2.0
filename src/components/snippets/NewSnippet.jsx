9f117c0c528e8ee8516bb4e8b8e2e4b32e32eb3b
zioan <zaharia.ioan@gmail.com>
2023-03-06 10:45:34 +0100

    fix No Tag typo that was alocationg the wrong tag on new snippet creation and cleanup

--- src/components/snippets/NewSnippet.jsx
+++ src/components/snippets/NewSnippet.jsx
@@ -1,63 +1,63 @@
-import { useState, useContext } from 'react';
-import SnippetContext from '../../context/SnippetContext';
-import TagContext from '../../context/TagContext';
-import CodeEditor from '@uiw/react-textarea-code-editor';
-import { FiSave } from 'react-icons/fi';
+import { useState, useContext } from 'react'
+import SnippetContext from '../../context/SnippetContext'
+import TagContext from '../../context/TagContext'
+import CodeEditor from '@uiw/react-textarea-code-editor'
+import { FiSave } from 'react-icons/fi'
 
 function NewSnippet() {
-  const [title, setTitle] = useState('');
-  const [code, setCode] = useState('');
-  const [selectedTag, setSelectedTag] = useState('No Tag');
-  const [error, setError] = useState('');
-  const [successMessage, setSuccessMessage] = useState('');
+  const [title, setTitle] = useState('')
+  const [code, setCode] = useState('')
+  const [selectedTag, setSelectedTag] = useState('No tag')
+  const [error, setError] = useState('')
+  const [successMessage, setSuccessMessage] = useState('')
 
-  const { newSnippet } = useContext(SnippetContext);
-  const { tags } = useContext(TagContext);
+  const { newSnippet } = useContext(SnippetContext)
+  const { tags } = useContext(TagContext)
 
   const saveSnippetHandler = (e) => {
-    e.preventDefault();
+    e.preventDefault()
 
     if (title.length < 1) {
-      setError('Title cannot be empty');
-      return;
+      setError('Title cannot be empty')
+      return
     }
 
     if (code.length < 1) {
-      setError('Code cannot be empty');
-      return;
+      setError('Code cannot be empty')
+      return
     }
 
     try {
-      newSnippet(title, selectedTag, code);
-      setError('');
-      setTitle('');
-      setCode('');
-      setSuccessMessage('Snippet created');
+      newSnippet(title, selectedTag, code)
+      setError('')
+      setTitle('')
+      setCode('')
+      setSuccessMessage('Snippet created')
       setInterval(() => {
-        setSuccessMessage('');
-      }, 4000);
+        setSuccessMessage('')
+      }, 4000)
     } catch (error) {
-      console.log(error);
+      console.log(error)
     }
-  };
+  }
 
   return (
-    <div className='mb-4 flex flex-col'>
-      <label className='block'>Create new snippet</label>
+    <div className="flex flex-col mb-4">
+      <label className="block">Create new snippet</label>
       <form onSubmit={saveSnippetHandler}>
         {/* Snippet title */}
         <input
-          type='text'
-          placeholder='Enter snippet title'
-          className='input input-bordered w-full max-w-xs block mb-4'
+          type="text"
+          placeholder="Enter snippet title"
+          className="block w-full max-w-xs mb-4 input input-bordered"
           value={title}
           onChange={(e) => setTitle(e.target.value)}
         />
 
         {/* Snippet tag */}
-        <label className='block'>Select tag</label>
+        <label className="block">Select tag</label>
         <select
-          className='select select-bordered w-full max-w-xs  mb-4'
+          className="w-full max-w-xs mb-4 select select-bordered"
           onChange={(e) => setSelectedTag(e.target.value)}
           value={selectedTag}
         >
@@ -66,20 +66,20 @@ function NewSnippet() {
               <option key={tag.id} value={tag.tag}>
                 {tag.tag}
               </option>
-            );
+            )
           })}
         </select>
 
         {/* Snippet code. react-textarea-code-editor component */}
-        <div className='max-h-96 overflow-auto'>
-          <label className='block'>Code editor</label>
+        <div className="overflow-auto max-h-96">
+          <label className="block">Code editor</label>
           <CodeEditor
             disabled={false}
-            className='code-editor '
+            className="code-editor "
             value={code}
             onChange={(e) => setCode(e.target.value)}
-            language='jsx'
-            placeholder='Paste your code here'
+            language="jsx"
+            placeholder="Paste your code here"
             padding={15}
             style={{
               fontFamily:
@@ -89,17 +89,17 @@ function NewSnippet() {
         </div>
 
         {/* Display errors if empty fields */}
-        {error && <p className=' text-red-400 text-center mt-4'>{error}</p>}
+        {error && <p className="mt-4 text-center text-red-400 ">{error}</p>}
         {successMessage && (
-          <p className=' text-success text-center mt-4'>{successMessage}</p>
+          <p className="mt-4 text-center text-success">{successMessage}</p>
         )}
 
-        <button className='btn mt-4' type='submit'>
-          <FiSave className=' text-2xl ' />
+        <button className="mt-4 btn" type="submit">
+          <FiSave className="text-2xl " />
         </button>
       </form>
     </div>
-  );
+  )
 }
 
-export default NewSnippet;
+export default NewSnippet
