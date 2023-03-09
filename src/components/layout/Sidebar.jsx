736141d88e48b7bb1e61294e9498e320733752fb
zioan <zaharia.ioan@gmail.com>
2023-03-09 16:18:18 +0100

    integrating new snippet and tags components with modal to the sidebar

--- src/components/layout/Sidebar.jsx
+++ src/components/layout/Sidebar.jsx
@@ -1,13 +1,36 @@
 import React from 'react'
+import Modal from '../../helpers/Modal'
 import FilterByTag from '../snippets/FilterByTag'
+import SnippetModal from '../snippets/modals/SnippetModal'
+import TagsModal from '../snippets/modals/TagsModal'
+import NewSnippet from '../snippets/NewSnippet'
+import NewTag from '../snippets/NewTag'
 import Search from '../snippets/Search'
+import TagsEditor from '../snippets/TagsEditor'
 
 function Sidebar() {
   const footerYear = new Date().getFullYear()
 
   return (
     <div className="w-[400px] h-[calc(100vh-64px)] border-r-2 p-4 self-start top-[64px] sticky flex flex-col justify-between">
-      <div>
+      <div className="mt-10">
+        <Modal buttonName={'Create new Snippet'}>
+          <NewTag />
+          <NewSnippet />
+        </Modal>
+        <Modal buttonName={'Tags Editor'}>
+          <>
+            <h3 className="mb-8 text-2xl text-center ">Tags Editor</h3>
+            <NewTag />
+
+            <h4>Click tags to delete</h4>
+            <div className="flex flex-wrap gap-2 mt-4 ">
+              <TagsEditor />
+            </div>
+          </>
+        </Modal>
+        <SnippetModal />
+        <TagsModal />
         <FilterByTag />
         <Search />
       </div>
