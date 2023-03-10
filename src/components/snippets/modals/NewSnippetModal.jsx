095f312fcbecb78a40f0aaf7caef6960e59985d8
zioan <zaharia.ioan@gmail.com>
2023-03-10 11:58:10 +0100

    extract new snippet and tags editor from snippets page and use suspense to wait for data

---
+++ src/components/snippets/modals/NewSnippetModal.jsx
@@ -0,0 +1,55 @@
+import { useRef } from 'react'
+import { VscNewFile } from 'react-icons/vsc'
+import NewSnippet from '../NewSnippet'
+import NewTag from '../NewTag'
+
+function NewSnippetModal() {
+  const snippetRef = useRef()
+
+  const closeSnippetsModal = (e) => {
+    if (e.key === 'Escape') {
+      snippetRef.current.click()
+    }
+  }
+
+  return (
+    <div>
+      <label
+        htmlFor="my-modal-1"
+        className="fixed z-20 btn btn-success btn-circle modal-button bottom-48 right-4"
+      >
+        <VscNewFile className="text-2xl " />
+      </label>
+
+      {/*  Put this part before </body> tag  */}
+      <input
+        type="checkbox"
+        id="my-modal-1"
+        className="modal-toggle "
+        onKeyDown={closeSnippetsModal}
+      />
+      <div className="modal ">
+        <div
+          className="relative modal-box box-shadow"
+          onKeyDown={closeSnippetsModal}
+        >
+          {/* Button to close modal */}
+          <label
+            htmlFor="my-modal-1"
+            className="absolute btn btn-sm btn-circle right-2 top-2"
+            ref={snippetRef}
+          >
+            ✕
+          </label>
+
+          {/* Modal content */}
+          <h3 className="mb-8 text-2xl text-center ">Add new snippet</h3>
+          <NewTag />
+          <NewSnippet />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default NewSnippetModal
---
+++ src/components/snippets/modals/TagsModal.jsx
@@ -0,0 +1,58 @@
+import { useRef } from 'react'
+import { BsTags } from 'react-icons/bs'
+import NewTag from '../NewTag'
+import TagsEditor from '../TagsEditor'
+
+function TagsModal() {
+  const tagRef = useRef()
+  const closeTagsModal = (e) => {
+    if (e.key === 'Escape') {
+      tagRef.current.click()
+    }
+  }
+
+  return (
+    <div>
+      <label
+        htmlFor="my-modal-2"
+        className="fixed z-20 btn btn-success btn-circle modal-button bottom-28 right-4"
+      >
+        <BsTags className="text-2xl " />
+      </label>
+
+      {/*  Put this part before </body> tag  */}
+      <input
+        type="checkbox"
+        id="my-modal-2"
+        className="modal-toggle"
+        onKeyDown={closeTagsModal}
+      />
+      <div className="modal">
+        <div
+          className="relative modal-box box-shadow"
+          onKeyDown={closeTagsModal}
+        >
+          {/* Button to close modal */}
+          <label
+            htmlFor="my-modal-2"
+            className="absolute btn btn-sm btn-circle right-2 top-2"
+            ref={tagRef}
+          >
+            ✕
+          </label>
+
+          {/* Modal content */}
+          <h3 className="mb-8 text-2xl text-center ">Tags Editor</h3>
+          <NewTag />
+
+          <h4>Click tags to delete</h4>
+          <div className="flex flex-wrap gap-2 mt-4 ">
+            <TagsEditor />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default TagsModal
