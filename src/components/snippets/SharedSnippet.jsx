d6ffcaeba84e6498033966a97a5c9a1fac952f6d
zioan <zaharia.ioan@gmail.com>
2024-11-26 13:01:46 +0100

    Increase shared page margin-top and linting

--- src/components/snippets/SharedSnippet.jsx
+++ src/components/snippets/SharedSnippet.jsx
@@ -1,54 +1,47 @@
-import { useContext, useEffect } from 'react'
-import { Link, useParams } from 'react-router-dom'
-import SnippetContext from '../../context/SnippetContext'
-import CodeEditor from '@uiw/react-textarea-code-editor'
-import Spinner from '../layout/Spinner'
-import { FiCopy } from 'react-icons/fi'
-import AuthContext from '../../context/AuthContext'
+import { useContext, useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
+import SnippetContext from "../../context/SnippetContext";
+import CodeEditor from "@uiw/react-textarea-code-editor";
+import Spinner from "../layout/Spinner";
+import { FiCopy } from "react-icons/fi";
+import AuthContext from "../../context/AuthContext";
 
 function SharedSnippet() {
-  const { user } = useContext(AuthContext)
+  const { user } = useContext(AuthContext);
 
-  const { sharedSnippet, getSharedSnippet, loading } = useContext(
-    SnippetContext,
-  )
+  const { sharedSnippet, getSharedSnippet, loading } = useContext(SnippetContext);
 
-  const params = useParams()
+  const params = useParams();
 
   //read url params and send params to SnippetContext for fetching
   useEffect(() => {
-    getSharedSnippet(params.user_name, params.user_id, params.snippet_id)
-  }, [])
+    getSharedSnippet(params.user_name, params.user_id, params.snippet_id);
+  }, []);
 
-  const saveToClipboard = () =>
-    navigator.clipboard.writeText(sharedSnippet[0].code)
+  const saveToClipboard = () => navigator.clipboard.writeText(sharedSnippet[0].code);
 
   return (
-    <>
+    <div className="mt-[100px]">
       {loading && <Spinner />}
 
       {/* If user logged in */}
       {user && (
-        <div className="mt-10">
+        <div className="mt-[100px]">
           <h4 className="text-xl ">
-            {user.name}, you are logged in,{' '}
+            {user.name}, you are logged in,{" "}
             <Link className="mx-2 underline text-success" to="/snippets">
               go back to your snippets
             </Link>
             .
           </h4>
-          <h4 className="text-xl ">
-            This page is designed for sharing your snippets.
-          </h4>
-          <h4 className="text-xl ">
-            Feel free to share this link with the world.
-          </h4>
+          <h4 className="text-xl ">This page is designed for sharing your snippets.</h4>
+          <h4 className="text-xl ">Feel free to share this link with the world.</h4>
         </div>
       )}
 
       {/* If no user */}
       {!user && (
-        <div className="my-10">
+        <div className="mt-10">
           <h4 className="text-2xl ">
             New to Snippets? Give it a try and
             <Link className="mx-2 underline text-success" to="/">
@@ -61,15 +54,10 @@ function SharedSnippet() {
 
       {/* If link is valid and snippet is found in DB */}
       {sharedSnippet && (
-        <div className="flex flex-col mt-10 ">
+        <div className="flex flex-col my-10 ">
           <div className="flex flex-col md:flex-row md:justify-between">
-            <h2 className="mb-4 text-2xl underline ">
-              {sharedSnippet[0].title}
-            </h2>
-            <div
-              className="inline tooltip justify-self-end "
-              data-tip="Copy code"
-            >
+            <h2 className="mb-4 text-2xl underline ">{sharedSnippet[0].title}</h2>
+            <div className="inline tooltip justify-self-end " data-tip="Copy code">
               <button className="btn btn-success " onClick={saveToClipboard}>
                 <FiCopy className="text-2xl " />
               </button>
@@ -83,8 +71,7 @@ function SharedSnippet() {
               language="jsx"
               padding={15}
               style={{
-                fontFamily:
-                  'ui-monospace,SFMono-Regular,SF Mono,Consolas,Liberation Mono,Menlo,monospace',
+                fontFamily: "ui-monospace,SFMono-Regular,SF Mono,Consolas,Liberation Mono,Menlo,monospace",
               }}
             />
           </div>
@@ -92,13 +79,9 @@ function SharedSnippet() {
       )}
 
       {/* If link is no valid and no data gfetched */}
-      {!sharedSnippet && (
-        <h4 className="text-2xl ">
-          The snippet you are looking for does not exist!
-        </h4>
-      )}
-    </>
-  )
+      {!sharedSnippet && <h4 className="text-2xl ">The snippet you are looking for does not exist!</h4>}
+    </div>
+  );
 }
 
-export default SharedSnippet
+export default SharedSnippet;
