06c194e0ece3168ed31c6f87204e534f6f850258
zioan <zaharia.ioan@gmail.com>
2025-01-28 13:51:46 +0100

    Add new backend server url

--- src/server.js
+++ src/server.js
@@ -1,4 +1,2 @@
 // // backend server
-export default process.env.NODE_ENV === 'development'
-  ? 'http://localhost:4000'
-  : process.env.NODE_ENV === 'production' && 'https://snippetsserver.zioan.com'
+export default process.env.NODE_ENV === "development" ? "http://localhost:4000" : process.env.NODE_ENV === "production" && "http://localhost:8004";
