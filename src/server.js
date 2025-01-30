33bd5340d30910438c38ee3d90c0dd97d8501a78
zioan <zaharia.ioan@gmail.com>
2025-01-30 08:36:09 +0100

    Set a new backend url

--- src/server.js
+++ src/server.js
@@ -1,4 +1,4 @@
 // // backend server
 export default process.env.NODE_ENV === "development"
   ? "http://localhost:4000"
-  : process.env.NODE_ENV === "production" && "https://api.snippets.zioan.com";
+  : process.env.NODE_ENV === "production" && "https://snippets_api.zioan.com";
